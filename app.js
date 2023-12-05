11d25f9bc8a6deb1ae4c6a7607598e7eb6b60953
Namy <damienbiron33@gmail.com>
2023-12-05 14:16:51 +0100

    aucune mise a jour

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const toggleButton = document.querySelector('.toggle-button')
-const navLinks = document.querySelector('.nav-links')
-const navButton = document.querySelectorAll('.button')
+const toggleButton = document.querySelector('.toggle-button');
+const navLinks = document.querySelector('.nav-links');
+const navButton = document.querySelectorAll('.button');
 
 // ca fonctionne
 toggleButton.addEventListener('click', () => {
-    toggleButton.classList.toggle('active')
-    navLinks.classList.toggle('active')
-    navButton.classList.toggle('active')
-})  
+  toggleButton.classList.toggle('active');
+  navLinks.classList.toggle('active');
+  navButton.classList.toggle('active');
+});
 
-// a tester 
+// a tester
 // toggleButton.addEventListener('click', () => {
 //     navLinks.classList.toggle("close")
 // })
-
 
 // a tester
 // navButton.addEventListener('click', () => {
@@ -21,19 +20,15 @@ toggleButton.addEventListener('click', () => {
 //     toggleButton.classList.remove("active")
 // })
 
-
-
-
 let map = L.map('map').setView([51.505, -0.09], 9);
 
 let Stadia_OSMBright = L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
-    {
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    }
-  );
+  'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
+  {
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  }
+);
 
-
-  Stadia_OSMBright.addTo(map);
+Stadia_OSMBright.addTo(map);
