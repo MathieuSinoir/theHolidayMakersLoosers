5e76035dc2ad7e5ad8fc38a6d983fec065396572
yohan59235 <yohan.collinse@gmail.com>
2023-12-07 11:28:27 +0100

    j'espère que ca sera le dernier!

--- app.js
+++ app.js
@@ -23,22 +23,14 @@ function closePopup() {
 
 const map = L.map('map').setView([51.505, -0.09], 3);
 
-
-let map = L.map('map').setView([51.505, -0.09], 9);
-
-
 const Stadia_OSMBright = L.tileLayer(
 
   'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
   {
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 20, 
   },
 );
 
-
-stadia_OSMBright.addTo(map);
 Stadia_OSMBright.addTo(map);
 
 const uk = L.marker([51.5, -0.09]).addTo(map);
